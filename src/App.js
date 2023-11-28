471e04e87fffc53478e474a38d11c3fffb73230e
Paola Vidal <paola@Paolas-MacBook-Air.local>
2023-11-28 12:51:40 +0100

    added component to app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
       <Header title="Mouse Tracker" initialCount={0} />
+      <Footer /> 
     </div>
   );
 }
