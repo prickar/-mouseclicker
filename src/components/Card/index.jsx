3154e5118eb08cb21e9570aae328a3055891688b
Paola Vidal <paola@Paolas-MacBook-Air.local>
2023-11-30 13:11:18 +0100

    adding random stars and last test

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,6 +1,22 @@
 import ClickyButton from "../ClickyButton"
+import Star from '../../assets/icons8-hand-drawn-star-24.png'
+import { useState } from "react"
 
 const Card = ({ setCount }) => {
+
+	const [stars, setstars] = useState([])
+
+	const handleClick = () => {
+	    setCount((prevCount) => prevCount + 1)
+
+	const newStar = {
+		id: stars.lenght + 1,
+		top: `${Math.random() * window.innerHeight}px`,
+		left: `${Math.random() * window.innerWidth}px`,
+	}
+
+	setstars([...stars, newStar])
+}
 	return (
 		<>
 			<div data-testid="card">
@@ -9,10 +25,20 @@ const Card = ({ setCount }) => {
 				<ClickyButton />
 				<button
 					name="Get a Star"
-					onClick={() => setCount((prevCount) => prevCount + 1)}
+					onClick={handleClick}
 				>
 					Get a Star
 				</button>
+				{stars.map((star, index) => (
+          <img
+            key={index}
+            src={Star}
+            alt="Star"
+            className="absolute"
+            style={{ top: star.top, left: star.left }}
+          />
+        ))}
+
 			</div>
 		</>
 	)
