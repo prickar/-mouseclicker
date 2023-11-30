51e01bbe744c1dd8f8773e28c62952345ea32069
Freddie CME <freddie.kaplan@edu.cmeducations.se>
2023-11-30 15:31:43 +0100

    remove input.css.

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,46 +1,40 @@
 import ClickyButton from "../ClickyButton"
-import Star from '../../assets/icons8-hand-drawn-star-24.png'
+import Star from "../../assets/icons8-hand-drawn-star-24.png"
 import { useState } from "react"
 
 const Card = ({ setCount }) => {
-
 	const [stars, setstars] = useState([])
 
 	const handleClick = () => {
-	    setCount((prevCount) => prevCount + 1)
+		setCount((prevCount) => prevCount + 1)
 
-	const newStar = {
-		id: stars.lenght + 1,
-		top: `${Math.random() * window.innerHeight}px`,
-		left: `${Math.random() * window.innerWidth}px`,
+		const newStar = {
+			id: stars.lenght + 1,
+			top: `${Math.random() * window.innerHeight}px`,
+			left: `${Math.random() * window.innerWidth}px`,
+		}
+
+		setstars([...stars, newStar])
 	}
-
-	setstars([...stars, newStar])
-}
 	return (
 		<>
 			<div data-testid="card">
 				<h3>How it Works</h3>
 				<p data-testid="infotext">Click the button to see more stars</p>
 				<ClickyButton />
-				<button
-					className="btn"
-					name="Get a Star"
-					onClick={handleClick}
-				>
+				<button className="btn" name="Get a Star" onClick={handleClick}>
 					Get a Star
 				</button>
-				
-				{stars.map((star, index) => (
-          <img
-            key={index}
-            src={Star}
-            alt="Star"
-            className="absolute"
-            style={{ top: star.top, left: star.left }}
-          />
-        ))}
 
+				{stars.map((star, index) => (
+					<img
+						key={index}
+						src={Star}
+						alt="Star"
+						className="absolute"
+						style={{ top: star.top, left: star.left }}
+					/>
+				))}
 			</div>
 		</>
 	)
